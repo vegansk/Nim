71033e26a9032adce395f7caa6a89fd98067b154
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2015-11-25 15:16:24 +0600

    Removed unneded constraint check call

--- compiler/sigmatch.nim
+++ compiler/sigmatch.nim
@@ -1676,7 +1676,7 @@ proc matchesAux(c: PContext, n, nOrig: PNode,
           setSon(m.call, formal.position + 1, arg)
           inc(f)
           container = nil
-      checkConstraint(n.sons[a])
+        checkConstraint(n.sons[a])
     inc(a)
 
 proc semFinishOperands*(c: PContext, n: PNode) =
